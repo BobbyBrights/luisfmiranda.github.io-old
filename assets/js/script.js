c7edd614b17e40881e4d90e96006701614bc87c8
Luis <luisfmir@gmail.com>
2019-07-04 14:19:31 +0100

    Start sending onboarding info to the mobile layer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,31 @@
 let continueButtonIsActive = false;
 
 function handleInput() {
-    const emailCharsCount = document.getElementById("email").value.length;
+    const nameCharsCount = document.getElementById("name").value.length;
     const passwordCharsCount = document.getElementById("password").value.length;
 
     try {
-        if (emailCharsCount !== 0 && passwordCharsCount !== 0) {
+        if (nameCharsCount !== 0 && passwordCharsCount !== 0) {
             if (!continueButtonIsActive) {
-                console.log('Continue button: on');
-                window.webkit.messageHandlers.iOSCreateAccountButton.postMessage(true);
+                window.webkit.messageHandlers.iOSToggleCreateAccountButton.postMessage(true);
                 continueButtonIsActive = true
             }
         } else {
             if (continueButtonIsActive) {
-                console.log('Continue button: off');
-                window.webkit.messageHandlers.iOSCreateAccountButton.postMessage(false);
+                window.webkit.messageHandlers.iOSToggleCreateAccountButton.postMessage(false);
                 continueButtonIsActive = false
             }
         }
+    } catch (err) {
+        console.log('Unable to reach the mobile layer:', err);
+    }
+}
+
+function retrieveUserInfo() {
+    const name = document.getElementById("name").value;
+
+    try {
+        window.webkit.messageHandlers.iOSSendUserInfo.postMessage(name);
     } catch (err) {
         console.log('Unable to reach the mobile layer:', err);
     }
