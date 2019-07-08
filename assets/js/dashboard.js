f5adc8353d06e9c55c033861666357b2938199d4
Luis <luisfmir@gmail.com>
2019-07-08 17:26:08 +0100

    Fix the issue with the "goToServices" button (on the dashboard screen)

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,6 +1,7 @@
 function goToServicesButtonClicked() {
     try {
-        window.webkit.messageHandlers.iOSGoToServices.postMessage();
+        // the postMessage function must send something, even if it's null
+        window.webkit.messageHandlers.iOSGoToServices.postMessage(null);
     } catch (err) {
         console.log('Unable to reach the mobile layer:', err);
     }
