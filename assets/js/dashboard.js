0aaa004dab09e7d5ac375ba1acb33ed20572c276
Luis <luisfmir@gmail.com>
2019-07-10 17:05:35 +0100

    Test the API that fetches the exchange rates

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,3 +1,30 @@
+﻿let exchangeRates = {
+    'gbp/usd': 0
+};
+
+function updateExchangeRates() {
+    fetchPrices();
+}
+
+function fetchPrices() {
+    // build and send the request
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://rate-client.eu-west-1.elasticbeanstalk.com/getrate');
+    xhr.send();
+
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            console.log("Fetched.")
+        } else {
+            console.log(xhr);
+        }
+    };
+
+    xhr.onerror = () => {
+        console.log("Unable to fetch prices.")
+    };
+}
+
 function goToServicesButtonClicked() {
     try {
         // the postMessage function must send something, even if it's null
@@ -6,3 +33,4 @@ function goToServicesButtonClicked() {
         console.log('Unable to reach the mobile layer:', err);
     }
 }
+
