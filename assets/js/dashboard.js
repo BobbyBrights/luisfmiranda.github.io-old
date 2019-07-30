6b0a1e2980742d2e370bb3a360109fdeac78649b
Luis <luisfmir@gmail.com>
2019-07-30 11:58:10 +0100

    Adapt the dashboard to work with Android

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -77,10 +77,14 @@ function selectUsd() {
   baseCcy = 'usd'
 }
 
-function goToServicesButtonClicked() {
+function seeAllRatesButtonClicked() {
     try {
-        // the postMessage function must send something, even if it's null
-        window.webkit.messageHandlers.iOSGoToServices.postMessage(null);
+        if (typeof window.webkit !== 'undefined') { // iOS
+            // the postMessage function must send something, even if it's null
+            window.webkit.messageHandlers.iOSGoToServices.postMessage(null);
+        } else if (typeof androidLayer !== 'undefined') { // Android
+            androidLayer.seeAllRates();
+        }
     } catch (err) {
         console.log('Unable to reach the mobile layer:', err);
     }
